bd98f4c0b51bbc3e56f7354efc7dbcc48f2892b8
Nicholas <nicholas.coltinne@gmail.com>
2014-10-14 14:54:24 -0300

    Revert "5.0.2"
    This reverts commit 3b82a81f87fb9fea86c9f6e88abcc18a5d77011b.

--- paginas/ModificarClassificacao.aspx.cs
+++ paginas/ModificarClassificacao.aspx.cs
@@ -7,46 +7,8 @@ using System.Web.UI.WebControls;
 
 public partial class paginas_ModificarClassificacao : System.Web.UI.Page
 {
-    Mod_modelos modelo; // Cria variavel do tipo Questionario
-    Clas_classificacoes classificacao;
-    Clas_classificacoes classificacaoN;
-
-    int index;
-
     protected void Page_Load(object sender, EventArgs e)
     {
-        modelo = (Mod_modelos)Session["modelo"];
-        index = (Int32)Session["index"];
-        classificacao = (Clas_classificacoes)modelo.Classificacoes[index];
-        if (!IsPostBack)
-        {
-            valorarCampos();
-        }
-    }
 
-    private void salvarClassificacao()
-    {
-        classificacaoN = new Clas_classificacoes();
-        classificacaoN.NomeClassificacao = txtTitulo.Text;
-        classificacaoN.DescricaoClassificacao = txtClassificacao.Value;
-        classificacaoN.PontoClassificacao = Convert.ToDouble(txtPontuacao.Value);
-        modelo.Classificacoes[index] = classificacaoN;
-        Session["modelo"] = modelo;
-    }
-
-    private void valorarCampos()
-    {
-        txtTitulo.Text = classificacao.NomeClassificacao;
-        txtClassificacao.Value = classificacao.DescricaoClassificacao;
-        txtPontuacao.Value = classificacao.PontoClassificacao.ToString();
-    }
-    protected void btnConfirmar_Click(object sender, EventArgs e)
-    {
-        salvarClassificacao();
-        Response.Redirect("ConfirmarModelo.aspx");
-    }
-    protected void btnCancelar_Click(object sender, EventArgs e)
-    {
-        Response.Redirect("ConfirmarModelo.aspx");
     }
 }
--- paginas/ModificarModelo.aspx.cs
+++ paginas/ModificarModelo.aspx.cs
@@ -7,36 +7,8 @@ using System.Web.UI.WebControls;
 
 public partial class paginas_ModificarModeloEd : System.Web.UI.Page
 {
-    Mod_modelos modelo; // Cria variavel do tipo Questionario
-
     protected void Page_Load(object sender, EventArgs e)
     {
-        modelo = (Mod_modelos)Session["modelo"]; //Instancia o obj Questionario, com os valores vindos da pg anterior
-        if (!IsPostBack)
-        {
-            valorarCampos();
-        }
-    }
 
-    private void salvarModelo()
-    {
-        modelo.NomeModelo = txb_nomeModelo.Value.ToString();
-        modelo.DescricaoModelo = txt_descricao.Value.ToString();
-        Session["modelo"] = modelo;
-    }
-
-    private void valorarCampos()
-    {
-        txb_nomeModelo.Value = modelo.NomeModelo.ToString();
-        txt_descricao.Value = modelo.DescricaoModelo.ToString();
-    }
-    protected void btn_continuar_Click(object sender, EventArgs e)
-    {
-        salvarModelo();
-        Response.Redirect("ConfirmarModelo.aspx");
-    }
-    protected void btn_cancelar_Click(object sender, EventArgs e)
-    {
-        Response.Redirect("ConfirmarModelo.aspx");
     }
 }
